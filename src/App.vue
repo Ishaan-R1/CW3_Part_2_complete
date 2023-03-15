e216b8baf920f6d66c88090de34e896dc9aab684
Ishaan-R1 <119463909+Ishaan-R1@users.noreply.github.com>
2023-03-15 16:38:36 +0000

    Refactoring App to include features from CW1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
-<!-- <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script> -->
-
 <template>
   <div id="app">
-    <!-- <link rel="stylesheet" href="style.css" /> -->
-    <!-- <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header> -->
-    <!-- <header>
-      <h1>{{ sitename }}</h1>
-
-      <button @click="showCheckout()">{{ this.cart.length }} Checkout</button>
-    </header> -->
     <h1>{{ sitename }}</h1>
-    <button @click="showCheckout()" id="CartBtn">
+    <button @click="showCheckout()" v-if="canViewCart" id="CartBtn">
       <!-- Displaying cart icon on button -->
       <img src="Images/cart.png" alt="Cart" class="cart" />
       {{ cartItemCount }}
       Checkout
     </button>
+    <!-- Creating disabled button when no items are added to cart -->
+    <button class="disabled" id="CartBtnDisabled" disabled='disabled' v-else>
+        <img src="Images/cart.png" alt="Cart" class="cart2">
+        Checkout
+      </button>
     <button
       class="testConsoleBtn"
       v-if="testConsole"
@@ -43,6 +30,53 @@ import TheWelcome from './components/TheWelcome.vue'
       <strong class="testConsole">HTTPS Test: </strong>
       <a v-bind:href="awsServerURL" target="_blank">link</a>
     </div>
+    <!-- <div class="order">
+        <p>Order:<br></p>
+        <form>
+            <input type="radio" id="a" name="fav_language" value="Ascending" v-on:click="ascend()">
+            <label for="html">Ascending</label><br>
+            <input type="radio" id="d" name="fav_language" value="Descending" v-on:click="descend()">
+            <label for="css">Descending</label><br>
+        </form>
+      </div> -->
+
+      <!-- <form>
+        <div class="container2">
+          <div class="sort">
+            <p>Sort By:<br></p>
+            <div v-if="this.sortOrder.order === 'ascend'">
+              <input type="radio" id="subject" name="fav_language" value="Subject" v-model="radioBtn"
+                v-on:click="sort('subject')">
+                  <label for="subject">Subject</label><br>
+                <input type="radio" id="subject" name="fav_language" value="Location" v-model="radioBtn"
+                v-on:click="sort('location')">
+                <label for="subject">Location</label><br>
+                <input type="radio" id="subject" name="fav_language" value="Price" v-model="radioBtn"
+                v-on:click="sort('price')">
+                <label for="subject">Price</label><br>
+                <input type="radio" id="subject" name="fav_language" value="Availability" v-model="radioBtn"
+                v-on:click="sort('spaces')">
+                <label for="subject">Availability</label><br>
+                
+            </div>
+            <div v-else>
+              <input type="radio" id="subject" name="fav_language" value="Subject" v-model="radioBtn"
+                v-on:click="sort2('subject')">
+                <label for="subject">Subject</label><br>
+              <input type="radio" id="subject" name="fav_language" value="Location" v-model="radioBtn"
+                v-on:click="sort2('location')">
+                <label for="subject">Location</label><br>
+                <input type="radio" id="subject" name="fav_language" value="Price" v-model="radioBtn"
+                v-on:click="sort2('price')">
+                <label for="subject">Price</label><br>
+                <input type="radio" id="subject" name="fav_language" value="Availability" v-model="radioBtn"
+                v-on:click="sort2('spaces')">
+                <label for="subject">Availability</label><br>
+            </div>
+          </div>
+        </div>
+
+      </form> -->
 
     <main>
       <component
@@ -50,7 +84,15 @@ import TheWelcome from './components/TheWelcome.vue'
         :sortedProducts="sortedProducts"
         :cart="cart"
         @add-item-to-cart="addToCart"
+        @sort-item="sort"
+        @sort-item2="sort2"
         @manage-remove-lesson="manageRemoveLesson"
+        :order="order"
+        :checkForm="checkForm"
+        :orderMsg="orderMsg"
+        :sortOrder="sortOrder"
+        :ascend="ascend"
+        :descend="descend"
       ></component>
     </main>
     <!-- <main>
@@ -79,8 +121,25 @@ export default {
       subjects: [],
       getID: [],
       getSpaces: [],
-      //subjects: subjects,
-    };
+      sortOrder: {
+      order: "ascend",
+      },
+      radioBtn: {
+        option: "",
+      },
+      // Object to hold data of message when order is successful
+      orderMsg: {
+        msg: "Thank for your order. Your order has been placed.",
+      },
+      // Object to hold data of user inputted order details
+      order: {
+        firstName: "",
+        lastName: "",
+        address: "",
+        phone: "",
+      },
+        //subjects: subjects,
+      };
   },
   components: { Lesson, Checkout },
   created: function () {
@@ -100,6 +159,25 @@ export default {
     // setInterval(this.searchLessons, 1000);
   },
   methods: {
+    sort: function (option) {
+      // create function to compare each element
+      function compare(a, b) {
+        if (a[option] > b[option]) return 1;
+        if (a[option] < b[option]) return -1;
+        return 0;
+      }
+      this.subjects.sort(compare); // sort all the already compared elements
+    },
+    sort2: function (option) {
+      // create function to compare each element
+      function compare(a, b) {
+        if (a[option] > b[option]) return -1;
+        if (a[option] < b[option]) return 1;
+        return 0;
+      }
+      // sort all the already compared elements
+      this.subjects.sort(compare);
+    },
     postOrder(firstName, phone_number, id) {
       const order = {
         name: firstName,
@@ -169,53 +247,47 @@ export default {
       console.log(this.cart);
       console.log("Added to Arr " + this.cart.length);
     },
+    ascend: function () {
+      this.sortOrder.order = "ascend";
+      console.log(this.sortOrder.order);
+    },
+    descend: function () {
+      this.sortOrder.order = "descend";
+      console.log(this.sortOrder.order);
+    },
   },
   computed: {
+    // Computed property to check if checkout button can be enabled or not
+    canViewCart: function () {
+      return this.cartItemCount >= 1; // check if number of items in cart array is more than or equal to 1
+    },
     // Computed property to check how many items are added to cart array
     cartItemCount: function () {
       // Displays number of items in cart array or returns empty string if array is empty
       return this.cart.length || "";
     },
+    // Method to sort subjects in ascended order
     sortedProducts() {
-      function compare(a, b) {
-        if (a.price > b.price) return +1;
-        if (a.price < b.price) return -1;
-        return 0;
-      }
-      return this.subjects.sort(compare);
+      return this.subjects;
     },
     oneItemInCart: function () {
       return this.cartItemCount >= 1;
     },
+    // Computed property to check if checkout  button on checkout page can be enabled or not
+    checkForm: function () {
+      return (
+        // Regular expressions to check if the first and last name contains only letters
+        /^[a-zA-Z]+$/.test(this.order.firstName) &&
+        /^[a-zA-Z]+$/.test(this.order.lastName) &&
+        // Regular expressions to check if the phone number contains only numbers
+        /^[0-9]+$/.test(this.order.phone) &&
+        // Check if all fields are inputted
+        this.order.firstName.length > 0 &&
+        this.order.lastName.length > 0 &&
+        this.order.address.length > 0 &&
+        this.order.phone.length > 0
+      );
+    },
   },
 };
 </script>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
